f9beae559073e9ca51cd76fb08feedc8b1e7c11c
konrad.konieczny <konrad.konieczny@solteq.com>
2024-03-27 17:01:48 +0100

    test-actions: test change

--- Notification/Model/Message/VersionNotification.php
+++ Notification/Model/Message/VersionNotification.php
@@ -14,6 +14,7 @@ class VersionNotification implements MessageInterface
 
     /**
      * VersionNotification constructor.
+     * test chagne 2
      *
      * @param Session $authSession
      * @param InboxFactory $inboxFactory
