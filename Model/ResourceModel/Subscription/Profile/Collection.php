8d493226cf589a5142a3b0ab945d23311954fb82
Konrad Konieczny <konrad.konieczny@solteq.com>
2025-05-15 16:00:26 +0200

    SQMAGOPC-651: refactor: improve type hinting for SearchCriteriaInterface and update return types in Collection class

--- Model/ResourceModel/Subscription/Profile/Collection.php
+++ Model/ResourceModel/Subscription/Profile/Collection.php
@@ -2,6 +2,7 @@
 
 namespace Paytrail\PaymentService\Model\ResourceModel\Subscription\Profile;
 
+use Magento\Framework\Api\SearchCriteriaInterface;
 use Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection;
 use Paytrail\PaymentService\Api\Data\RecurringProfileSearchResultInterface;
 use Paytrail\PaymentService\Model\Subscription\Profile;
@@ -9,9 +10,12 @@ use Paytrail\PaymentService\Model\ResourceModel\Subscription\Profile as ProfileR
 
 class Collection extends AbstractCollection implements RecurringProfileSearchResultInterface
 {
-    /** @var \Magento\Framework\Api\SearchCriteriaInterface */
+    /** @var SearchCriteriaInterface */
     private $searchCriteria;
 
+    /**
+     * Initialize the collection.
+     */
     protected function _construct()
     {
         $this->_init(
@@ -24,7 +28,8 @@ class Collection extends AbstractCollection implements RecurringProfileSearchRes
      * Set items list.
      *
      * @param \Magento\Framework\DataObject[] $items
-     * @return \Paytrail\PaymentService\Model\ResourceModel\Subscription\Profile\Collection
+     *
+     * @return Collection
      * @throws \Exception
      */
     public function setItems(array $items = [])
@@ -38,16 +43,24 @@ class Collection extends AbstractCollection implements RecurringProfileSearchRes
         return $this;
     }
 
+    /**
+     * Search criteria getter.
+     *
+     * @return SearchCriteriaInterface
+     */
     public function getSearchCriteria()
     {
         return $this->searchCriteria;
     }
 
     /**
-     * @param \Magento\Framework\Api\SearchCriteriaInterface $searchCriteria
+     * Search criteria setter.
+     *
+     * @param SearchCriteriaInterface $searchCriteria
+     *
      * @return $this|Collection
      */
-    public function setSearchCriteria(\Magento\Framework\Api\SearchCriteriaInterface $searchCriteria)
+    public function setSearchCriteria(SearchCriteriaInterface $searchCriteria)
     {
         $this->searchCriteria = $searchCriteria;
 
@@ -68,7 +81,8 @@ class Collection extends AbstractCollection implements RecurringProfileSearchRes
      * Set total count.
      *
      * @param int $totalCount
-     * @return \Paytrail\PaymentService\Model\ResourceModel\Subscription\Profile\Collection
+     *
+     * @return Collection
      * @SuppressWarnings(PHPMD.UnusedFormalParameter)
      */
     public function setTotalCount($totalCount)
