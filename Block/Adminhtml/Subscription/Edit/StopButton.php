8831a8c8bffcae24ba1a512ce84d6623e1b0888f
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2026-05-18 14:22:25 +0200

    SQMAGOPC-683: remove Stop Schedule button for not active subscriptions

--- Block/Adminhtml/Subscription/Edit/StopButton.php
+++ Block/Adminhtml/Subscription/Edit/StopButton.php
@@ -2,12 +2,26 @@
 
 namespace Paytrail\PaymentService\Block\Adminhtml\Subscription\Edit;
 
+use Magento\Backend\Block\Widget\Context;
+use Magento\Framework\App\RequestInterface;
+use Paytrail\PaymentService\Api\Data\SubscriptionInterface;
+use Paytrail\PaymentService\Api\SubscriptionRepositoryInterface;
+
 class StopButton extends AbstractButton
 {
+    public function __construct(
+        Context $context,
+        RequestInterface $request,
+        private SubscriptionRepositoryInterface $subscriptionRepository
+    ) {
+        parent::__construct($context, $request);
+    }
+
     public function getButtonData()
     {
         $data = [];
-        if ($this->getId()) {
+        $subscriptionStatus = $this->subscriptionRepository->get($this->getId())->getStatus();
+        if ($this->getId() && $subscriptionStatus === SubscriptionInterface::STATUS_ACTIVE) {
             $data = [
                 'label' => __('Stop schedule'),
                 'class' => 'delete',
