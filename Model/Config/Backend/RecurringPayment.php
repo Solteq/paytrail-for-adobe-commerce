2976e64fde04a75c5c0f14255dec1c14e0d7c8c0
Konrad Konieczny <konrad.konieczny@solteq.com>
2026-07-30 09:56:04 +0200

    SQMAGOPC-692: fix comment

--- Model/Config/Backend/RecurringPayment.php
+++ Model/Config/Backend/RecurringPayment.php
@@ -8,6 +8,8 @@ use Magento\Framework\Exception\LocalizedException;
 class RecurringPayment extends Value
 {
     /**
+     * Not allowed to enable recurring payments when "Payment method selection on a separate page" is enabled.
+     *
      * @throws LocalizedException
      */
     public function beforeSave()
