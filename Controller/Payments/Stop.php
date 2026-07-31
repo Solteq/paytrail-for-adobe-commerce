34642c7d19c609d849f84642a532cd8a555a1d87
Konrad Konieczny <konrad.konieczny@solteq.com>
2026-07-31 16:13:28 +0200

    SQMAGOPC-692: add status to stop action and update logic

--- Controller/Payments/Stop.php
+++ Controller/Payments/Stop.php
@@ -20,6 +20,7 @@ use Paytrail\PaymentService\Api\SubscriptionLinkRepositoryInterface;
 use Paytrail\PaymentService\Api\SubscriptionRepositoryInterface;
 use Paytrail\PaymentService\Model\SubscriptionManagement;
 use Paytrail\PaymentService\Model\Validation\PreventAdminActions;
+use Paytrail\PaymentService\Setup\Patch\Data\PendingSubscriptionStatus;
 use Psr\Log\LoggerInterface;
 
 class Stop implements Action\HttpGetActionInterface
@@ -38,15 +39,15 @@ class Stop implements Action\HttpGetActionInterface
      * @param ManagerInterface $messageManager
      */
     public function __construct(
-        private Context                             $context,
-        private Session                             $customerSession,
-        private SubscriptionRepositoryInterface     $subscriptionRepositoryInterface,
-        private OrderRepositoryInterface            $orderRepositoryInterface,
-        private OrderManagementInterface            $orderManagementInterface,
-        private LoggerInterface                     $logger,
+        private Context $context,
+        private Session $customerSession,
+        private SubscriptionRepositoryInterface $subscriptionRepositoryInterface,
+        private OrderRepositoryInterface $orderRepositoryInterface,
+        private OrderManagementInterface $orderManagementInterface,
+        private LoggerInterface $logger,
         private SubscriptionLinkRepositoryInterface $subscriptionLinkRepositoryInterface,
-        private PreventAdminActions                 $preventAdminActions,
-        private ManagerInterface                    $messageManager
+        private PreventAdminActions $preventAdminActions,
+        private ManagerInterface $messageManager
     ) {
     }
 
@@ -69,7 +70,7 @@ class Stop implements Action\HttpGetActionInterface
 
         try {
             $subscription = $this->subscriptionRepositoryInterface->get((int)$subscriptionId);
-            $orderIds     = $this->subscriptionLinkRepositoryInterface->getOrderIdsBySubscriptionId(
+            $orderIds = $this->subscriptionLinkRepositoryInterface->getOrderIdsBySubscriptionId(
                 (int)$subscriptionId
             );
 
@@ -78,10 +79,16 @@ class Stop implements Action\HttpGetActionInterface
                 if (!$this->customerSession->getId() || $this->customerSession->getId() != $order->getCustomerId()) {
                     throw new LocalizedException(__('Customer is not authorized for this operation'));
                 }
-                $subscription->setStatus(SubscriptionInterface::STATUS_CLOSED);
-                if ($order->getStatus() === Order::STATE_PENDING_PAYMENT
-                    || $order->getStatus() === SubscriptionManagement::ORDER_PENDING_STATUS) {
-                    $this->orderManagementInterface->cancel($order->getId());
+                try {
+                    if ($order->getStatus() === Order::STATE_PENDING_PAYMENT
+                        || $order->getStatus() === SubscriptionManagement::ORDER_PENDING_STATUS
+                        || $order->getStatus() === PendingSubscriptionStatus::ORDER_STATUS_PENDING_SUBSCRIPTION
+                    ) {
+                        $this->orderManagementInterface->cancel($order->getId());
+                    }
+                } catch (\Exception $e) {
+                    $this->logger->error($e->getMessage());
+                    $this->messageManager->addErrorMessage(__('Unable to cancel order with ID: %1', $orderId));
                 }
             }
 
