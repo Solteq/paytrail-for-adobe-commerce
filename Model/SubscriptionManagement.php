a43db6161c316d3ba03bd725a47e8afa10d85b81
Bartek Kałużny <bartosz.kaluzny@solteq.com>
2026-09-18 12:08:26 +0200

    SQMAGOPC-692: remove empty line

--- Model/SubscriptionManagement.php
+++ Model/SubscriptionManagement.php
@@ -56,7 +56,6 @@ class SubscriptionManagement implements SubscriptionManagementInterface
         private CustomerData                        $customerData,
         private ShowSubscriptionsDataProvider       $showSubscriptionsDataProvider
     ) {
-
     }
 
     /**
